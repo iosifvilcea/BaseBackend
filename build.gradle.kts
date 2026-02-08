b08d829179eca84ae8eb361bb105dc8e239acb8b
iosifvilcea <iosifvilcea@protonmail.com>
2026-02-08 07:06:26 +0200

    Add the correct path of env props to gitignore. Import sb starter mail and update app.properties with profiles.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.postgresql:postgresql")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
